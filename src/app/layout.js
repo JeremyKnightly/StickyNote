c7f5a345d07b8387f0b56e2569a0970cfc5f450a
Jeremy Chevalier <jeremy.chevalier9494@gmail.com>
2024-06-19 23:55:22 -0600

    testing if app is broken without analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +13,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics/>
       </body>
     </html>
   );
