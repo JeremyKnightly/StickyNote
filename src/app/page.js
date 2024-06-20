95a7a21e817a9d23ec9edd34cc748479864369eb
Jeremy Chevalier <jeremy.chevalier9494@gmail.com>
2024-06-20 00:27:43 -0600

    Hopefully reverted to the not broken version

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,17 @@
 "use client"
 
-import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 import NoteList from "./components/NoteList";
 import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
+import { nanoid } from "nanoid";
 
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [searchParam, setSearchParam] = useState("");
-  const localStorage = typeof window !== "undefined" ? window.localStorage : null;
-  const [notes, setNotes] = useState(
-    localStorage.getItem('react-sticky-notes-app-data') ? 
-    JSON.parse(localStorage.getItem('react-sticky-notes-app-data')) 
-    : []
-  );
+  //const [localStorage, setLocalStorage] = useState();
+  const [notes, setNotes] = useState([]);
 
   function addNote(text) {
     const date = new Date();
@@ -40,11 +36,29 @@ export default function Home() {
     addNote(text);
   }
 
+
+
+  useEffect(() =>{
+    console.log(JSON.stringify(notes));
+
+      const savedNotes = JSON.parse(
+        localStorage.getItem('react-sticky-notes-app-data')
+      );
+
+      if (savedNotes) {
+        console.log("load successful:" + JSON.stringify(savedNotes));
+        setNotes(savedNotes);
+      }
+
+  },[]);
+
   useEffect(() => {
+    console.log("load successful:" + JSON.stringify(notes));
     localStorage.setItem(
       'react-sticky-notes-app-data', 
       JSON.stringify(notes)
     );
+    console.log("Save successful:" + JSON.stringify(notes));
   },[notes]);
 
   return (
